1fe7ebe9f8d2e735f6aa7789ae00319e1d5103fa
Winflexsa <persico.matias@gmail.com>
2026-09-25 12:25:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const C='pendientes-v1';
+const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=='GET'||u.origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;})
+    .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
